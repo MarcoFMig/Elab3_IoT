0ae339593814eb0674adc90d0198e51e2671d919
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-08-08 19:45:14 +0200

    Fixed style and stoplight events

--- River-monitoring/src/resources/javascript/titlebarmanager.js
+++ River-monitoring/src/resources/javascript/titlebarmanager.js
@@ -15,6 +15,7 @@ class Stoplight {
     this.minimizeBtn = minimizeBtn;
     this.resizeBtn = resizeBtn;
     this.exitBtn = exitBtn;
+    this.maximized = false;
 
     minimizeBtn.onclick = () => {
       this.minimizeListeners.forEach(listener => listener());
@@ -22,7 +23,15 @@ class Stoplight {
     };
     resizeBtn.onclick = () => {
       this.resizeListeners.forEach(listener => listener());
-      mainIC.windowCtrl.sendMaximize();
+      if (this.maximized) {
+        mainIC.windowCtrl.sendMinimize();
+        this.resizeBtn.getElementsByTagName("img")[0].src = "./resources/images/vector/four-corners-arrows-line-icon-white.svg"
+        this.maximized = false;
+      } else {
+        mainIC.windowCtrl.sendMaximize();
+        this.resizeBtn.getElementsByTagName("img")[0].src = "./resources/images/vector/four-arrows-inside-line-icon-white.svg"
+        this.maximized = true;
+      }
     };
     exitBtn.onclick = () => {
       this.closeListeners.forEach(listener => listener());
@@ -30,17 +39,32 @@ class Stoplight {
     };
   }
 
+  /**
+   * Registers a listener for when the window is minimized.
+   * @param {function(*): void} listener  the listener that will be called
+   */
   onMinimize(listener) {
     this.minimizeListeners.push(listener);
   }
+  /**
+   * Registers a listener for when the window is maximized / minimized.
+   * @param {function(*): void} listener the listener that will be called
+   */
   onResize(listener) {
     this.resizeListeners.push(listener);
   }
+  /**
+   * Register a listener for when the window is closed.
+   * @param {function(*): void} listener the listener that will be called
+   */
   onClose(listener) {
     this.closeListeners.push(listener);
   }
 }
 
+/**
+ * Title bar manager namespace.
+ */
 const titleBarManager = {
   init: () => {
 
