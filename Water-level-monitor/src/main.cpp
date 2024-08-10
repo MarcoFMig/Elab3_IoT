5b88484d08e21f3f6c66031b8144240f3bbc2f68
Leonardo Marcaccio <leonardo.marcaccio@studio.unibo.it>
2024-08-10 18:31:59 +0200

    Water Level Monitor first implementation

--- Water-level-monitor/src/main.cpp
+++ Water-level-monitor/src/main.cpp
@@ -1,18 +1,122 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "Led.hpp"
+#include "Sonar.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
+#define F1 (uint8_t) 10
+#define F2 (uint8_t) 100
+#define LED_ON true
+#define LED_OFF false
+#define LED_PIN_GREEN (uint8_t) 5
+#define LED_PIN_RED (uint8_t) 6
+#define MSG_BUFFER_SIZE  50
+#define SONAR_ECHO_PIN (uint8_t) 7 
+#define SONAR_TRIGGER_PIN (uint8_t) 8
+
+
+/* wifi network info */
+const char* ssid; //INSERISCI NOME RETE WI-FI
+const char* password; //INSERISCI PASSWORD RETE WI-FI
+
+/* MQTT server address */
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+
+/* MQTT topic */
+const char* topic = "esiot-2023";
+
+/* MQTT client management */
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+unsigned long lastMsgTime = 0;
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+Led greenLed;
+Led redLed;
+uint8_t scanDelay;
+
+void setup_wifi() {
+  delay(10);
+  Serial.println(String("Connecting to ") + ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/* MQTT subscribing callback */ //RICEZIONE MESSAGGIO
+void callback(char* topic, byte* payload, unsigned int length) {
+  scanDelay = 1000/F2;
+  Serial.println(String("Message arrived on [") + topic + "] len: " + length );
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    // Create a random client ID
+    String clientId = String("esiot-2122-client-")+String(random(0xffff), HEX);
+
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      // client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe(topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void updateLedStates(bool isNetOk, bool isSendDataOk) {
+  if (isNetOk && isSendDataOk) {
+    greenLed.setToggle(LED_ON);
+    redLed.setToggle(LED_OFF);
+  } else {
+    greenLed.setToggle(LED_OFF);
+    redLed.setToggle(LED_ON);
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  setup_wifi();
+  randomSeed(micros());
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  greenLed = Led(LED_PIN_GREEN);
+  redLed = Led(LED_PIN_RED);
+  scanDelay = 1000/F1;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  delay(scanDelay);
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  snprintf(msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+  Serial.println(String("Publishing message: ") + msg);
+  /* publishing the msg */
+  client.publish(topic, msg);
 }
