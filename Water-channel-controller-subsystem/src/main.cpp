ba606ef863178cda5c3fbf4f919164058534fbbb
Lorenzo Dalmonte <lorenzo.dalmonte4@studio.unibo.it>
2024-08-16 16:12:39 +0200

    Updated the code for for controller subsystem

--- Water-channel-controller-subsystem/src/main.cpp
+++ Water-channel-controller-subsystem/src/main.cpp
@@ -13,10 +13,15 @@
 #define ANGLE_100 180
 #define BUF_SIZE 30
 
+const char valveOpening[] PROGMEM = "Valve opening:";
+const char automatic[] PROGMEM = "AUTOMATIC";
+const char manual[] PROGMEM = "MANUAL";
+
 Servo servo;
 Button button = Button();
 Potentiometer pot = Potentiometer();
 LCDDisplay lcd = LCDDisplay();
+String tmp;
 char buffer[BUF_SIZE];
 uint8_t angle;
 uint8_t perc;
@@ -28,8 +33,10 @@ void setup() {
 
 void loop() {
   if (button.getCurrentState() == STATE_AUTO) {
-    Serial.readString().toCharArray(buffer, BUF_SIZE);
-    if (strncmp(buffer, "Valve opening", 13) == 0) {
+    while (Serial.available() == 0) {}
+    tmp = Serial.readString();
+    strncpy(buffer, tmp.c_str(), BUF_SIZE);
+    if (strncmp(buffer, valveOpening, 14) == 0) {
         switch (buffer[16])
         {
             case 0:
@@ -46,15 +53,15 @@ void loop() {
                 break;
         }
         lcd.write(buffer);
-        lcd.concat(F("AUTOMATIC"));
+        lcd.concat(automatic);
     }
   } else {
     perc = pot.getValue();
     angle = map(perc, 0, 100, 0, 180);
-    snprintf(buffer, BUF_SIZE, "Valve opening: %hhu %c", perc, '%');
+    snprintf(buffer, BUF_SIZE, "%s %hhu %c", valveOpening, perc, '%');
     lcd.write(buffer);
-    lcd.concat(F("MANUAL"));
+    lcd.concat(manual);
   }
   servo.write(angle);
-  delay(2000);
+  delay(1000);
 }
