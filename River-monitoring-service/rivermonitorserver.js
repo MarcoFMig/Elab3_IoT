58ff6ce33cd8f5af17669a09fc8ae7bca2709680
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-08-18 18:30:24 +0200

    Implemented COM handling for Arduino

--- River-monitoring-service/rivermonitorserver.js
+++ River-monitoring-service/rivermonitorserver.js
@@ -1,5 +1,9 @@
 const http = require("http");
 const mqtt = require('mqtt');
+const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+
+// MQTT PART
 
 class SimpleMQTTConnection {
   connectListeners = new Array();
@@ -93,6 +97,74 @@ class SimpleMQTTConnection {
     this.errorListeners.push(listener);
   }
 }
+
+// END MQTT PART
+
+// COM PART
+
+let comSessions = []
+
+/**
+ * Checks if a string starts with any of the strings passed.
+ * @param {*} str the string to check
+ * @param {*} substrs a set of starting points
+ * @returns returns true if any of the substrings are
+ * 			present at the start of the given string
+ */
+function stringStartsWith(string, stringSet) {
+	return stringSet.some(substr => string.startsWith(substr));
+}
+
+const comManager = {
+	listConnectedDevices: () => {
+		return SerialPort.list()
+	},
+	generateComSession: (path, baudrate, openFunc, dataFunc) => {
+		let ses = new COMSession(path, baudrate, openFunc, dataFunc)
+		comSessions.push(ses)
+		return comSessions.indexOf(ses)
+	},
+	startComSession: (sessionId) => {
+		comSessions[sessionId].startConnection()
+	},
+	sendMessageToComSession: (sessionId, data, errorHandler) => {
+		comSessions[sessionId].sendMessage(data, errorHandler)
+	}
+}
+
+class COMSession {
+	constructor(path, baudrate, openFunc, dataFunc) {
+		this.path = path
+		this.baudrate = baudrate
+		this.openFunc = openFunc
+		this.dataFunc = dataFunc
+	}
+
+	startConnection() {
+		this.serialConnPort = new SerialPort({
+			path:this.path,
+			baudRate:this.baudrate
+		})
+		const dataParser = this.serialConnPort.pipe(
+			new ReadlineParser({ delimiter: '\n' }))
+			this.serialConnPort.on("open", () => {
+			this.openFunc()
+		});
+		dataParser.on('data', (data) =>{
+			this.dataFunc(data)
+		});
+	}
+	
+	sendMessage(data, errorHandler) {
+		this.serialConnPort.write(data, function(error) {
+			if (error) {
+			  return console.log('Error on write: ', err.message)
+			}
+		  })
+	}
+}
+
+// END COM PART
 
 const host = 'localhost';
 const port = 8123;
