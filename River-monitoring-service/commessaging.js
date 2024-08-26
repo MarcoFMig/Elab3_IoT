ce151510d018edb294fb5f735c14469d82c85635
Marco Francolini <38810810+MarcoFMig@users.noreply.github.com>
2024-08-26 22:29:46 +0200

    Finished COM parser
    Co-authored-by: Lorenzo Dalmonte <Dalmont-code@users.noreply.github.com>
    Co-authored-by: LeonardoMarcaccio <LeonardoMarcaccio@users.noreply.github.com>

--- River-monitoring-service/commessaging.js
+++ River-monitoring-service/commessaging.js
@@ -1,108 +1,62 @@
-const DEFAULT_SEPARATOR = "-";
-const DEFAULT_ASSIGNER = ":";
 const Author = {
   WATER_CHANNEL_CONTROLLER: 0b0,
   RIVER_MONITORING_SERVICE: 0b1
 }
 const DataTypes = {
-  WATER_LEVEL_INFO: "wli"
+  VALVE_FLOW: 0b0
 }
 const ControlTypes = {
-  PING: "PING",
-  PONG: "PONG"
+  PING: 0b0,
+  PONG: 0b1
 }
 const MessageTypes = {
   CONTROL: 0b0,
   DATA: 0b1
 }
-const DEFAULT_MESSAGE_TYPE_LENGTH = 4;
 
-class RiverMonitorData {
-  constructor(author, data) {
-    this.author = author;
-    this.data = data;
-  }
+const Masks = {
+  MASK_AUTHOR: 0b00000100,
+  MASK_TYPE: 0b00000010,
+  MASK_LAST: 0b00000001
 }
 
-class MQTTMessageFactory {
-  constructor(separator = DEFAULT_SEPARATOR) {
-    this.separator = DEFAULT_SEPARATOR;
-  }
-  addPrefix(message) {
-    return Author.RIVER_MONITORING_SERVICE + this.separator + message;
-  }
-  generateSpacing(...data) {
-    let monolith = "";
-    data.forEach(singleData => {
-      monolith += singleData + this.separator;
-    });
-    return monolith.slice(0, monolith.length - 1);
-  }
-  makeData(...dataToSend) {
-    return this.addPrefix(MessageTypes.DATA + DEFAULT_SEPARATOR + this.generateSpacing(dataToSend));
-  }
-  makePing() {
-    return this.addPrefix(MessageTypes.CONTROL + DEFAULT_SEPARATOR + ControlTypes.PING);
+class Message {
+  constructor(messageType, contentType, content) {
+    this.messageType = messageType;
+    this.contentType = contentType;
+    this.content = content;
   }
 }
 
 /**
  * 
- * @param {string} message 
- */
-function levelZeroParser(message) {
-  if (message.startsWith(Author.WATER_LEVEL_MONITOR)) {
-    return message.slice(Author.WATER_LEVEL_MONITOR.length + 1);
-  } else {
-    return false;
-  }
-}
-/**
- * 
- * @param {string} message 
- */
-function levelOneParser(message) {
-  let messageType = retrieveMessageType(message);
-    // NOTE: the length is intentionally not decreased in order to consider the spacer
-  let trimmedMessage = message.slice(DEFAULT_MESSAGE_TYPE_LENGTH + 1);
-  return trimmedMessage;
-}
-/**
- * 
- * @param {string} message 
- */
-function retrieveMessageType(message) {
-  // NOTE: the length is intentionally not decreased in order to consider the spacer
-  switch (message.slice(0, DEFAULT_MESSAGE_TYPE_LENGTH)) {
-    case MessageTypes.CONTROL:
-      return MessageTypes.CONTROL;
-    case MessageTypes.DATA:
-      return MessageTypes.DATA;
-    default:
-      return false;
-  }
-}
-/**
- * 
- * @param {string} levelZeroMessage 
+ * @param {number[]} message 
  * @returns 
  */
-function parseMessage(levelZeroMessage) {
-  let levelOneMessage = levelZeroParser(levelZeroMessage);
-  if (levelOneMessage == false) {
+function parseMessage(message) {
+  if (message.length != 2) {
+    throw new Error("Invalid message has been recieved");
+  }
+  let header = message[0];
+  if ((header & Masks.MASK_AUTHOR) != 0) {
     return false;
   }
-  let levelTwoMessage = levelOneParser(levelOneMessage);
-  if (levelTwoMessage == false) {
-    return false;
+  let messageType = null;
+  let contentType = null;
+  let content = null;
+  if ((header & Masks.MASK_TYPE) == 0) {
+    messageType = MessageTypes.CONTROL;
+    if ((header & Masks.MASK_LAST) != 0) {
+      contentType = ControlTypes.PONG;
+    }
+  } else {
+    messageType = MessageTypes.DATA;
+    if ((header & Masks.MASK_LAST) == 0) {
+      contentType = DataTypes.VALVE_FLOW;
+      content = message[1];
+    }
   }
-  let dataMap = new Map();
-  let dataElements = levelTwoMessage.split(DEFAULT_SEPARATOR);
-  dataElements.forEach(value => {
-    let separatedValue = value.split(DEFAULT_ASSIGNER);
-    dataMap.set(separatedValue[0], separatedValue[1] == null ? null : separatedValue[1]);
-  });
-  return dataMap;
+  return new Message(messageType, contentType, content);
 }
 
 function isPong(message) {
@@ -114,16 +68,11 @@ function isPong(message) {
 }
 
 const MessageParser = {
-  /**
-   * 
-   * @param {string} message 
-   */
-  levelZeroParser: (message) => levelZeroParser(message),
-  levelOneParser: (message) => levelOneParser(message),
-  retrieveMessageType: (message) => retrieveMessageType(message),
   parseMessage: (message) => parseMessage(message),
   isPong: (message) => isPong(message)
 }
+const MessageFactory = {
+  generatePing: () => [(((Author.RIVER_MONITORING_SERVICE << 1) | MessageTypes.CONTROL) << 1) | ControlTypes.PING, 0b0, "\r"]
+}
 
-module.exports = { RiverMonitorData, MQTTMessageFactory, Author, DataTypes, ControlTypes, MessageTypes, DEFAULT_SEPARATOR, MessageParser }
-
+module.exports = { Author, DataTypes, ControlTypes, MessageTypes, MessageParser, MessageFactory }
